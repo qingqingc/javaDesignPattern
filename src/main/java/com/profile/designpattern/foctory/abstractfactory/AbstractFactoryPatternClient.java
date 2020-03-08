a5df39d82b6ada8dee3dce091e18d52009cc839e
zhuyuan <zhuyuan@localhost>
2020-03-08 15:21:10 +0800

    t2

--- src/main/java/com/profile/designpattern/foctory/abstractfactory/AbstractFactoryPatternClient.java
+++ src/main/java/com/profile/designpattern/foctory/abstractfactory/AbstractFactoryPatternClient.java
@@ -2,6 +2,6 @@ package com.profile.designpattern.foctory.abstractfactory;
 
 public class AbstractFactoryPatternClient {
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test r");
 	}
 }
