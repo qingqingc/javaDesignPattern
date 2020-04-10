5e82370c9378d35462e73e1f13d4226cbff324b0
zhuyuan <zhuyuan@localhost>
2020-04-10 20:54:55 +0800

    add chinese comments.

--- src/main/java/com/profile/designpattern/state/StatePatternClient.java
+++ src/main/java/com/profile/designpattern/state/StatePatternClient.java
@@ -1,5 +1,10 @@
 package com.profile.designpattern.state;
 
+/**
+ * 状态模式
+ *
+ *
+ */
 public class StatePatternClient {
 	public static void main(String[] args) {
 		Environment en = new Environment();
